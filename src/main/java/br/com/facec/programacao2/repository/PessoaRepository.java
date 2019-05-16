79abf0921377bb031bbdaa1a92240403aafc31b7
emmanuelneri <emmanuelnerisouza@gmail.com>
2019-05-15 22:04:18 -0300

    replicando na repository

--- src/main/java/br/com/facec/programacao2/repository/PessoaRepository.java
+++ src/main/java/br/com/facec/programacao2/repository/PessoaRepository.java
@@ -1,34 +1,68 @@
 package br.com.facec.programacao2.repository;
 
+import br.com.facec.programacao2.exceptions.ElementoNaoEncontradoExpcetion;
 import br.com.facec.programacao2.model.Pessoa;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class PessoaRepository implements CRUDRepository<Pessoa> {
 
+    private List<Pessoa> pessoas = new ArrayList<>();
+
     @Override
     public void criar(Pessoa pessoa) {
-        System.out.println("Pessoa criada com sucesso! : " + pessoa);
+        pessoa.setId(new Random().nextLong());
+        this.pessoas.add(pessoa);
+        System.out.println("Pessoa criada com sucesso! " + pessoa);
     }
 
     @Override
     public void atualizar(Pessoa pessoa) {
-        System.out.println("Pessoa atualizada com sucesso! : " + pessoa);
+        Integer posicaoPessoa = getPosicaoElemento(pessoa.getId());
+        this.pessoas.remove(posicaoPessoa.intValue());
+        this.pessoas.add(posicaoPessoa, pessoa);
+
+        System.out.println("Pessoa atualizada com sucesso! " + pessoa);
     }
 
-    @Override
-    public Pessoa buscarPorId(Long id) {
-        return null;
-    }
+    private Integer getPosicaoElemento(Long id) {
+        Integer posicaoPessoa = null;
 
-    @Override
-    public List<Pessoa> buscarTodos() {
-        return null;
+        for(int i = 0; i < pessoas.size(); i++) {
+            Pessoa pessoaDaLista = pessoas.get(i);
+            if(pessoaDaLista.getId().equals(id)) {
+                posicaoPessoa = i;
+                break;
+            }
+        }
+
+        if(posicaoPessoa == null) {
+            throw new ElementoNaoEncontradoExpcetion("Elemento não encontrado: " + id);
+        }
+        return posicaoPessoa;
     }
 
     @Override
     public void deletar(Long id) {
-        System.out.println("Pessoa excluída com sucesso! : " + id);
+        Integer posicaoPessoa = getPosicaoElemento(id);
+        this.pessoas.remove(posicaoPessoa.intValue());
+        System.out.println("Pessoa excluído com sucesso! " + id);
+    }
 
+    @Override
+    public Pessoa buscarPorId(Long id) {
+        for (Pessoa pessoa : pessoas) {
+            if (pessoa.getId().equals(id)) {
+                return pessoa;
+            }
+        }
+        throw new ElementoNaoEncontradoExpcetion("Elemento não encontrado: " + id);
+    }
+
+    @Override
+    public List<Pessoa> buscarTodos() {
+        return new ArrayList<>(pessoas);
     }
 }
